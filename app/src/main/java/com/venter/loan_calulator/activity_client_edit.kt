09a89505bc1cdc220bff6d73f6ef9a2cfa2f95f2
swapn <swapneel.janrao@gmail.com>
2021-11-28 22:10:55 +0530

    comment added

--- app/src/main/java/com/venter/loan_calulator/activity_client_edit.kt
+++ app/src/main/java/com/venter/loan_calulator/activity_client_edit.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 class activity_client_edit : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //
+
         setContentView(R.layout.activity_client_edit)
     }
 }
