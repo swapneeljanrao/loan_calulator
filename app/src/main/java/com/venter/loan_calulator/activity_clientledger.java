570ec0c94ef79c6d32d34cdf1a12b6bad29e7070
swapn <swapneel.janrao@gmail.com>
2022-01-13 11:30:35 +0530

    All Screen backgrounds changed

---
+++ app/src/main/java/com/venter/loan_calulator/activity_clientledger.java
@@ -0,0 +1,15 @@
+package com.venter.loan_calulator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_clientledger extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_clientledger);
+    }
+}
